64bf3a875b041633772e383289f3a1303d2dd3f3
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-03 13:59:56 -0700

    Added routes for screens

--- OneHacksFrontend/src/App.jsx
+++ OneHacksFrontend/src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./styles/App.css";
 
+import { Switch, Route } from "react-router-dom";
+import Landing from "./screens/Landing";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
+import Dashboard from "./screens/Dashboard";
+import NoteForm from "./screens/NoteForm";
+import Businesses from "./screens/Businesses";
+import Decision from "./screens/Decision.";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/decision" component={Decision} />
+      <Route exact path="/noteform" component={NoteForm} />
+      <Route exact path="/businesses" component={Businesses} />
+    </Switch>
+  );
 }
 
 export default App;
