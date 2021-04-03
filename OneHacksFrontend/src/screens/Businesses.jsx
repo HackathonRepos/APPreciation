08e9c7632dc51efd17bfed9714f073396d0cc575
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-03 13:45:02 -0700

    Added all potential screens

---
+++ OneHacksFrontend/src/screens/Businesses.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Businesses() {
+  return (
+    <div>
+      <p>Buisnesses</p>
+    </div>
+  );
+}
+
+export default Businesses;
---
+++ OneHacksFrontend/src/screens/Decision..jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Decision() {
+  return (
+    <div>
+      <p>Decision</p>
+    </div>
+  );
+}
+
+export default Decision;
