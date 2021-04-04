46d51c869b0e555ab0155518b10d2c73cf990f6f
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-04 15:11:04 -0700

    Rendered all cards with a mock response

--- APPreciationFrontend/src/components/BusinessCard.jsx
+++ APPreciationFrontend/src/components/BusinessCard.jsx
@@ -18,15 +18,11 @@ function BusinessCard(props) {
       w="300px"
       backgroundColor="white"
       borderRadius="20px"
-      p="10px"
+      p="15px"
       m="20px"
       boxShadow="2xl"
     >
-      <Image
-        src="http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg"
-        w="35px"
-        h="35px"
-      />
+      <Image src={props.imageUrl} w="35px" h="35px" />
       <Heading>{props.businessName}</Heading>
       <Divider />
       <Text marginTop="10px">{props.address}</Text>
@@ -38,9 +34,11 @@ function BusinessCard(props) {
           <ListItem key={index}>{method}</ListItem>
         ))}
       </UnorderedList>
-      <Button colorScheme="teal" size="lg" m="10px">
-        Get Details
-      </Button>
+      <Flex height="100%" alignItems="flex-end">
+        <Button colorScheme="teal" size="lg" m="10px">
+          Get Details
+        </Button>
+      </Flex>
     </Flex>
   );
 }
--- APPreciationFrontend/src/screens/Businesses.jsx
+++ APPreciationFrontend/src/screens/Businesses.jsx
@@ -4,7 +4,7 @@ import { stateList } from "../dummydata/stateList";
 import stateCities from "state-cities";
 import { capitalCase } from "capital-case";
 
-import { Flex, Text, Heading, Button, Select } from "@chakra-ui/react";
+import { Flex, Heading, Button, Select } from "@chakra-ui/react";
 import BusinessCard from "../components/BusinessCard";
 import { mockData } from "../dummydata/mockData";
 
@@ -12,12 +12,26 @@ function Businesses() {
   const [state, setGeoState] = useState("California");
   const [city, setCity] = useState("Mountain View");
   const [disabled, setDisabled] = useState(false);
-
+  const cards = mockData["businesses"].map((business, index) => (
+    <BusinessCard
+      businessName={business["name"]}
+      pickup={business["transactions"]}
+      imageUrl={business["image_url"]}
+      address={
+        business["location"]["address1"] +
+        ", " +
+        business["location"]["city"] +
+        ", " +
+        business["location"]["state"]
+      }
+    />
+  ));
   return (
-    <Flex flexDirection="column" height="100vh" backgroundColor="gray.100">
+    <Flex flexDirection="column" height="100%" backgroundColor="gray.100">
       <Flex>
         <Heading>View all businesses in the State of</Heading>
         <Select
+          flex="1"
           placeholder={state}
           onChange={(evt) => setGeoState(evt.target.value, setDisabled(true))}
           size="lg"
@@ -32,6 +46,7 @@ function Businesses() {
       <Flex>
         <Heading>And in the city of</Heading>
         <Select
+          flex="1"
           placeholder={capitalCase(city)}
           size="lg"
           onChange={(evt) => setCity(evt.target.value, setDisabled(false))}
@@ -44,23 +59,13 @@ function Businesses() {
         </Select>
       </Flex>
       <Flex>
-        <Text>{state}</Text>
-        <Text>{city}</Text>
         <Button colorScheme="teal" size="lg" disabled={disabled}>
           {disabled ? "Select Another City" : "View Businesses"}
         </Button>
       </Flex>
-      <BusinessCard
-        businessName={mockData["businesses"][0]["name"]}
-        address={
-          mockData["businesses"][0]["location"]["address1"] +
-          ", " +
-          mockData["businesses"][0]["location"]["city"] +
-          ", " +
-          mockData["businesses"][0]["location"]["state"]
-        }
-        pickup={mockData["businesses"][0]["transactions"]}
-      />
+      <Flex flexWrap="wrap" justifyContent="center">
+        {cards}
+      </Flex>
     </Flex>
   );
 }
