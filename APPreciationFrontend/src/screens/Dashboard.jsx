1cb4d868d0732210fdb0dfa4ef6033097ca23932
AydanPirani <aydanpirani@gmail.com>
2021-04-04 15:31:00 -0700

    Tried fixing dashboard

--- APPreciationFrontend/src/screens/Dashboard.jsx
+++ APPreciationFrontend/src/screens/Dashboard.jsx
@@ -3,18 +3,22 @@ import firebase from "../firebase";
 
 
 function Dashboard() {
-  let db = firebase.firestore()
-  let user_id = "TMTD86D3yZN3zfMT6xsgp0y4LTW2"
-  // let user_id = firebase.auth().currentUser
-  db.collection("users").doc(user_id).get()
-  .then(results => {
-    console.log(results.data())
-    console.log(results.data()["restaraunts"])
-  }).catch(e => console.log("error with getting restaraunts"))
 
+  async function DisplayRestaurants() {
+    let db = firebase.firestore();
+    let user_id = "TMTD86D3yZN3zfMT6xsgp0y4LTW2";
+    // let user_id = firebase.auth().currentUser;
+    
+    const data = await db.collection("users").doc(user_id).get();
+    const restaurants = (data.data())["restaurants"].map(i => {
+      <p> {i} </p>
+    });
+    return restaurants
+  }
   return (
     <div>
       <p>Dashboard</p>
+      {DisplayRestaurants()}
     </div>
   );
 }
