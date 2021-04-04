40b78bd0077b8cae45d1bac320e79dce626e0307
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-04 03:53:04 -0700

    Dynamically generated states and cities via select

--- APPreciationFrontend/src/screens/Businesses.jsx
+++ APPreciationFrontend/src/screens/Businesses.jsx
@@ -1,34 +1,21 @@
 import React, { useState } from "react";
 
-import axios from "axios";
-import {
-  Flex,
-  Text,
-  Heading,
-  Button,
-  Center,
-  Link,
-  Select,
-} from "@chakra-ui/react";
+import { Flex, Text, Heading, Button, Select } from "@chakra-ui/react";
 import { stateList } from "../components/stateList";
 import stateCities from "state-cities";
-
-function GenerateRestaraunts() {
-  var rest = [1, 2];
-  // TODO: Hit Anant with a POST request
-  return rest;
-}
 
 function Businesses() {
   const [state, setGeoState] = useState("California");
   const [city, setCity] = useState("Mountain View");
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <Flex flexDirection="column" height="100vh" backgroundColor="gray.100">
       <Flex>
         <Heading>View all businesses in the State of</Heading>
         <Select
           placeholder={state}
-          onChange={(evt) => setGeoState(evt.target.value)}
+          onChange={(evt) => setGeoState(evt.target.value, setDisabled(true))}
           size="lg"
         >
           {stateList.map((geoState, index) => (
@@ -40,13 +27,24 @@ function Businesses() {
       </Flex>
       <Flex>
         <Heading>And in the city of</Heading>
-        <Select placeholder={city} size="lg">
+        <Select
+          placeholder={city}
+          size="lg"
+          onChange={(evt) => setCity(evt.target.value, setDisabled(false))}
+        >
           {stateCities.getCities(state).map((cityItem, index) => (
             <option value={cityItem} key={index}>
               {cityItem}
             </option>
           ))}
         </Select>
+      </Flex>
+      <Flex>
+        <Text>{state}</Text>
+        <Text>{city}</Text>
+        <Button colorScheme="teal" size="lg" disabled={disabled}>
+          {disabled ? "Select Another City" : "View Businesses"}
+        </Button>
       </Flex>
     </Flex>
   );
