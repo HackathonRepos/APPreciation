9d3532618f52bd3911a6f629ba7cd4fb1e565fca
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-04 15:47:08 -0700

    Fixed await/async restaraunt bug

--- APPreciationFrontend/src/screens/Dashboard.jsx
+++ APPreciationFrontend/src/screens/Dashboard.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import firebase from "../firebase";
 
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { restaurantList: [] };
+  }
 
-function Dashboard() {
-
-  async function DisplayRestaurants() {
+  componentDidMount() {
+    this.displayRestaurants();
+  }
+  async displayRestaurants() {
     let db = firebase.firestore();
     let user_id = "TMTD86D3yZN3zfMT6xsgp0y4LTW2";
     // let user_id = firebase.auth().currentUser;
-    
+
     const data = await db.collection("users").doc(user_id).get();
-    const restaurants = (data.data())["restaurants"].map(i => {
-      <p> {i} </p>
-    });
-    return restaurants
+    const restaurants = data
+      .data()
+      ["restaurants"].map((place, index) => <p key={index}> {place} </p>);
+    this.setState({ restaurantList: restaurants });
+    return restaurants;
   }
-  return (
-    <div>
-      <p>Dashboard</p>
-      {DisplayRestaurants()}
-    </div>
-  );
+
+  render() {
+    return (
+      <div>
+        <p>Dashboard</p>
+        {this.state.restaurantList}
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
