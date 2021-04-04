6c24b8ce56c675cc143c2fbd56d635627418cac3
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-04 02:43:17 -0700

    Added option to select state and city

--- APPreciationFrontend/src/screens/Businesses.jsx
+++ APPreciationFrontend/src/screens/Businesses.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
 import axios from "axios";
+import {
+  Flex,
+  Text,
+  Heading,
+  Button,
+  Center,
+  Link,
+  Select,
+} from "@chakra-ui/react";
+import { stateList } from "../components/stateList";
+import stateCities from "state-cities";
 
 function GenerateRestaraunts() {
   var rest = [1, 2];
@@ -8,10 +20,35 @@ function GenerateRestaraunts() {
 }
 
 function Businesses() {
+  const [state, setGeoState] = useState("California");
+  const [city, setCity] = useState("Mountain View");
   return (
-    <div>
-      <p>Buisnesses</p>
-    </div>
+    <Flex flexDirection="column" height="100vh" backgroundColor="gray.100">
+      <Flex>
+        <Heading>View all businesses in the State of</Heading>
+        <Select
+          placeholder={state}
+          onChange={(evt) => setGeoState(evt.target.value)}
+          size="lg"
+        >
+          {stateList.map((geoState, index) => (
+            <option value={geoState} key={index}>
+              {geoState}
+            </option>
+          ))}
+        </Select>
+      </Flex>
+      <Flex>
+        <Heading>And in the city of</Heading>
+        <Select placeholder={city} size="lg">
+          {stateCities.getCities(state).map((cityItem, index) => (
+            <option value={cityItem} key={index}>
+              {cityItem}
+            </option>
+          ))}
+        </Select>
+      </Flex>
+    </Flex>
   );
 }
 
