dc2b8641d8f5367c63f0992cae43c909084a0f24
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-03 17:41:01 -0700

    Added styles for signup form

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { Flex, Text, Heading, Button, Center } from "@chakra-ui/react";
+import { UnlockIcon } from "@chakra-ui/icons";
 
 function SignUp() {
   return (
-    <div>
-      <p>Sign Up</p>
-    </div>
+    <Flex flexDirection="row" h="100vh">
+      <Flex flex="1" className="Landing-Background" />
+      <Flex
+        flex="4"
+        backgroundColor="teal.200"
+        color="teal.900"
+        justifyContent="center"
+      >
+        <Center>
+          <Flex flexDirection="column" marginRight="15px" marginLeft="15px">
+            <Heading size="4xl">Sign Up With Google</Heading>
+            <Flex justifyContent="flex-end">
+              <Text marginTop="40px">
+                We're excited for you to try this out!
+              </Text>
+            </Flex>
+            <Button colorScheme="teal" size="lg" marginTop="100px">
+              <UnlockIcon marginRight="30px" />
+              Sign Up With Google
+            </Button>
+          </Flex>
+        </Center>
+      </Flex>
+      <Flex flex="1" className="Landing-Background" />
+    </Flex>
   );
 }
 
