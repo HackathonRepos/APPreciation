538bd8a66790ca777c1d826eeea9d1287a80a322
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-03 21:41:56 -0700

    Added form styles and positioning

--- APPreciationFrontend/src/screens/NoteForm.jsx
+++ APPreciationFrontend/src/screens/NoteForm.jsx
@@ -1,21 +1,17 @@
 import React, { useState } from "react";
 
-import firebase from "firebase";
-import { useForm } from "react-hook-form";
 import {
   Flex,
   Text,
   Heading,
   Button,
   CloseButton,
-  Center,
   Link,
   Input,
   Textarea,
-  FormErrorMessage,
   FormLabel,
-  FormControl,
 } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 function NoteForm() {
   const [sender] = useState("Sender");
@@ -28,64 +24,82 @@ function NoteForm() {
       backgroundColor="gray.100"
       justifyContent="center"
     >
-      <Heading color="teal.600" marginBottom="10px">
-        Submit your appreciation/thank you note here.
-      </Heading>
-      <Text color="teal.500" marginBottom="30px">
-        Please fill out all the fields as they are necessary
-      </Text>
-      <Flex flexDirection="column">
-        <FormLabel htmlFor="sender" color="teal.500">
-          Sender
-        </FormLabel>
-        <Input
-          name="sender"
-          value={sender}
-          readOnly
-          color="teal.700"
-          borderColor="teal.600"
-          focusBorderColor="teal.700"
-          backgroundColor="teal.50"
-          fontWeight="bold"
-        />
-      </Flex>
-      <Flex flexDirection="column">
-        <FormLabel htmlFor="recipient" color="teal.500">
-          Recipient
-        </FormLabel>
-        <Input
-          name="recipient"
-          value={recipient}
-          readOnly
-          color="teal.700"
-          borderColor="teal.600"
-          focusBorderColor="teal.700"
-          backgroundColor="teal.50"
-          fontWeight="bold"
-        />
-      </Flex>
-      <Flex flexDirection="column">
-        <FormLabel htmlFor="recipient" color="teal.500">
-          Message
-        </FormLabel>
-        <Textarea
-          placeholder="Enter your note here"
-          value={message}
-          onChange={(evt) => setMessage(evt.target.value)}
-          color="teal.700"
-          borderColor="teal.600"
-          focusBorderColor="teal.700"
-          backgroundColor="teal.50"
-          fontWeight="bold"
-        />
-      </Flex>
-      <Flex>
+      <Flex
+        flexDirection="column"
+        maxWidth="1000px"
+        marginLeft="100px"
+        marginRight="100px"
+      >
         <Link href="/businesses">
-          <CloseButton size="lg" color="teal.300" />
+          <ArrowBackIcon
+            h="40px"
+            w="40px"
+            marginBottom="50px"
+            color="teal.300"
+          />
         </Link>
-        <Button colorScheme="teal" size="lg">
-          Button
-        </Button>
+        <Heading color="teal.600" marginBottom="10px">
+          Submit your appreciation/thank you note here.
+        </Heading>
+        <Text color="teal.500" marginBottom="30px">
+          Please fill out all the fields as they are necessary
+        </Text>
+        <Flex flexDirection="column" marginBottom="15px">
+          <FormLabel htmlFor="sender" color="teal.500">
+            Sender
+          </FormLabel>
+          <Input
+            name="sender"
+            value={sender}
+            readOnly
+            color="teal.700"
+            borderColor="teal.600"
+            focusBorderColor="teal.700"
+            backgroundColor="teal.50"
+            fontWeight="bold"
+          />
+        </Flex>
+        <Flex flexDirection="column" marginBottom="15px">
+          <FormLabel htmlFor="recipient" color="teal.500">
+            Recipient
+          </FormLabel>
+          <Input
+            name="recipient"
+            value={recipient}
+            readOnly
+            color="teal.700"
+            borderColor="teal.600"
+            focusBorderColor="teal.700"
+            backgroundColor="teal.50"
+            fontWeight="bold"
+          />
+        </Flex>
+        <Flex flexDirection="column" marginBottom="15px">
+          <FormLabel htmlFor="recipient" color="teal.500">
+            Message
+          </FormLabel>
+          <Textarea
+            placeholder="Enter your note here"
+            value={message}
+            onChange={(evt) => setMessage(evt.target.value)}
+            color="teal.700"
+            borderColor="teal.600"
+            focusBorderColor="teal.700"
+            backgroundColor="teal.50"
+            fontWeight="bold"
+          />
+        </Flex>
+        <Flex justifyContent="flex-end" alignItems="center">
+          <CloseButton
+            size="lg"
+            color="teal.300"
+            marginRight="15px"
+            onClick={() => setMessage("")}
+          />
+          <Button colorScheme="teal" size="lg" disabled={message.length === 0}>
+            Submit
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   );
