e5f4b21000c56d03f2de946980389a069eaf7765
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-04 17:33:10 -0700

    Added business modal screen and future submit screen

---
+++ APPreciationFrontend/src/components/BusinessModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BusinessModal() {
+  return (
+    <div>
+      <p>B Modal</p>
+    </div>
+  );
+}
+
+export default BusinessModal;
---
+++ APPreciationFrontend/src/screens/Submit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Submit() {
+  return (
+    <div>
+      <p>Submit</p>
+    </div>
+  );
+}
+
+export default Submit;
